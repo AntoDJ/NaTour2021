b9673e47ea5fe8bb3d7292b8bd400903f6252d3a
GaBelardo <76739279+GaBelardo@users.noreply.github.com>
2022-03-01 12:06:13 +0100

    Cambiamento a Controller per testing

--- app/src/main/java/Controller/Controller_Stub.java
+++ app/src/main/java/Controller/Controller_Stub.java
@@ -2,6 +2,15 @@ package Controller;
 
 public class Controller_Stub extends Controller{
 
+    private static Controller_Stub instance;
+    public Controller_Stub() {}
+    public static Controller_Stub getInstance() {
+        if (instance == null) {
+            instance = new Controller_Stub();
+        }
+        return(instance);
+    }
+
     @Override
     public void createPath(String nome, String descrizione, float durata, int difficolta, boolean access, String puntoiniziale, String coordinate){
     }
